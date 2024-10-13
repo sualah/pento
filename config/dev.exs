5b669a9ae44df8583413092c40edc67776603fae
sualah <sualahabdullai@gmail.com>
2024-10-13 18:58:12 +0000

    added a simple live view component

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ import Config
 # Configure your database
 config :pento, Pento.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "root",
   hostname: "localhost",
   database: "pento_dev",
   stacktrace: true,
